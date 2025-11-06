4b11a5d8abff7bcf07acd4a0d7929dc9f8d10974
Ori Riner <OriR@users.noreply.github.com>
2025-11-06 02:16:34 +0200

    last one

--- environments/executable_node.sh
+++
@@ -1,9 +0,0 @@
-cdnvm(){
-  cd $1
-  if [[ -f .nvmrc && -s .nvmrc && -r .nvmrc ]]; then
-    <.nvmrc nvm install;
-  elif [[ $(nvm current) != $(nvm version default) ]]; then
-    nvm use default;
-  fi
-}
-alias cd='cdnvm'
--- post-setup-osx-a-configure-nvm.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# Default packages that will always get installed whenever a new node version is installed.
-echo 'npmrc' > $NVM_DIR/default-packages
-
-# Get the latest node version so we'll have something to work with.
-nvm install --lts
